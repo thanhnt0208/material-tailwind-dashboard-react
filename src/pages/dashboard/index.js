deedf3b338774ff6941b0b0af6f41aea15e61698
Tie902 <tiendmps40555@gmail.com>
2025-07-06 19:59:33 +0700

    themxoaanh

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,5 +1,5 @@
-export * from "@/pages/dashboard/home";
-export * from "@/pages/dashboard/users";
-export * from "@/pages/dashboard/farms";
-export * from "@/pages/dashboard/Questions/Questions";
+export { default as Home } from "./home";
+export { default as Users } from "./users";
+export { default as Tables } from "./tables";
+export { default as AcceptFarm } from "./AcceptFarm";;
 
