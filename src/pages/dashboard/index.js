64302a9ef917ba239f3e9571ef26d50438e1d93f
Tie902 <tiendmps40555@gmail.com>
2025-07-10 10:58:21 +0700

    adminreports

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -5,5 +5,5 @@ export * from "@/pages/dashboard/farm/farms";
 export * from "@/pages/dashboard/Questions/Questions";
 export * from "@/pages/dashboard/VideoFarms/VideoFarms";
 export * from "@/pages/dashboard/post/Post";
-
+export * from "@/pages/dashboard/AdminReports"
 
