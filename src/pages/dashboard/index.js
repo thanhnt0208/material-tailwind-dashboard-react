2a42f4b49a753d4535a03b13a1ca8fd93ef87ccc
Tie902 <tiendmps40555@gmail.com>
2025-07-11 11:13:42 +0700

    dashboar

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,12 +1,12 @@
-export * from "@/pages/dashboard/home";
+export { default as Home } from "./home";
 export * from "@/pages/dashboard/Questions/answerstable";
-export * from "@/pages/dashboard/user/users"; 
 export * from "@/pages/dashboard/farm/farms";
 export * from "@/pages/dashboard/Questions/Questions";
 export * from "@/pages/dashboard/VideoFarms/VideoFarms";
 export * from "@/pages/dashboard/post/Post";
 export * from "@/pages/dashboard/AdminReports"
 export * from "@/pages/dashboard/AdminCommentPost/CommentPost";
+export * from "@/pages/dashboard/user/users"
 
 
 
